f3d7ec3e61eae1197d67ec59102ced99f7c2a717
Roy Gero <roy.gero.ii@gmail.com>
2022-05-26 10:09:47 -0400

    Refactoring this script into a class instance. This will allow me to easily define more parameters.

--- SpotifyObsessions.py
+++ SpotifyObsessions.py
@@ -7,70 +7,98 @@ from datetime import date
 #Importing my credentials
 from credientials import *
 
+class SpotifyObsession:
+    def __init__(self, API_KEY, API_SECRET, API_URI):
+        self.API_KEY = API_KEY
+        self.API_SECRET = API_SECRET
+        self.API_URI = API_URI
+
+        # Setting Defaults
+        self.targetPlaylistName = "Hot Songs"
+        self.targetUser = ""
+        self.tracks = []
+        self.targetLimit = 20
+        self.timeRange = "short_term"
+
+        # Setting Default Permissions
+        self.Track = namedtuple('Track', ['artist', 'track'])
+        self.Scope = 'user-top-read playlist-modify-public'
+
+        # Getting Default Spotify Instance
+        self.Spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=self.API_KEY, 
+                                                client_secret=self.API_SECRET, 
+                                                redirect_uri=self.API_URI,
+                                                scope=self.Scope
+                                            ))
+
+    def setUser(self, targetUser):
+        self.targetUser = targetUser
+        return self
+
+    def setPlaylistName(self, targetName):
+        self.targetPlaylistName = targetName
+        return self
+
+    def setTargetLimit(self, targetLimit):
+        self.targetLimit = targetLimit
+        return self
+
+    def setTimeRange(self, timeRange):
+        self.timeRange = timeRange
+        return self
+
+    def getTopTracks(self):
+        currentTracks = self.Spotify.current_user_top_tracks(limit=self.targetLimit, offset=0, time_range=self.timeRange)
+
+        listOfTracks=[]
+        for song in currentTracks["items"]:
+            artistName = song["artists"][0]["name"]
+            songName = song["name"]
+            listOfTracks.append( self.Track(artistName,songName) )
+        self.tracks = listOfTracks
+
+    def generateSpotifyPlaylist(self):
+        self.getTopTracks()
+
+        formatted_tracks = []
+        for t in self.tracks:
+            try:
+                formatted_tracks.append(u'artist:"{artist}" track:"{track}"'.format(artist=t.artist, track=t.track))
+            except UnicodeDecodeError:
+                pass
+        search_res = [self.Spotify.search(q=t, type='track', limit=1) for t in formatted_tracks]
+        track_ids = [(first(r.get('tracks', {}).get('items', {})) or {}).get('uri') for r in search_res if
+                    r.get('tracks', {}).get('items')]
+
+        token = spotipy.util.prompt_for_user_token(self.targetUser, scope=self.Scope, 
+                                                            client_id=self.API_KEY, 
+                                                            client_secret=self.API_SECRET, 
+                                                            redirect_uri=self.API_URI)
+
+        if token:
+            # Recreate the Spotify instance because we have a new token with more permissions.
+            self.Spotify = spotipy.Spotify(auth=token)
+            self.Spotify.trace = False
+            playlist = self.Spotify.user_playlist_create(self.targetUser, self.targetPlaylistName, description=date.today().isoformat())
+
+            if playlist and playlist.get('id'):
+                self.Spotify.user_playlist_add_tracks(self.targetUser, playlist.get('id'), track_ids)
+                print("Playlist has been processed.")
+        else:
+            print("Can't get token for", self.targetUser)
+
+
+
+
+'''         Getting set up          '''
 SPOTIFY_API_KEY = credientials["SPOTIFY_API_KEY"]
 SPOTIFY_API_SECRET = credientials["SPOTIFY_API_SECRET"]
 SPOTIFY_URI = credientials["SPOTIFY_URI"]
-
-Track = namedtuple('Track', ['artist', 'track'])
-scope = 'user-top-read playlist-modify-public'
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_API_KEY, 
-                                                client_secret=SPOTIFY_API_SECRET, 
-                                                redirect_uri=SPOTIFY_URI,
-                                                scope=scope
-                                            ))
-
-def generate_spotify_playlist(tracks, playlist_name, username):
-    """
-    Generates a Spotify playlist from the given tracks
-    :param tracks: list of Track objects
-    :param playlist_name: name of playlist to create
-    :param username: Spotify username
-    """
-    formatted_tracks = []
-    for t in tracks:
-        try:
-            formatted_tracks.append(u'artist:"{artist}" track:"{track}"'.format(artist=t.artist, track=t.track))
-        except UnicodeDecodeError:
-            pass
-    search_res = [sp.search(q=t, type='track', limit=1) for t in formatted_tracks]
-    track_ids = [(first(r.get('tracks', {}).get('items', {})) or {}).get('uri') for r in search_res if
-                 r.get('tracks', {}).get('items')]
-
-    token = spotipy.util.prompt_for_user_token(username, scope=scope, 
-                                                         client_id=SPOTIFY_API_KEY, 
-                                                         client_secret=SPOTIFY_API_SECRET, 
-                                                         redirect_uri=SPOTIFY_URI)
-
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        sp.trace = False
-        playlist = sp.user_playlist_create(username, playlist_name, description=date.today().isoformat())
-
-        if playlist and playlist.get('id'):
-            sp.user_playlist_add_tracks(username, playlist.get('id'), track_ids)
-            print("Playlist has been processed.")
-    else:
-        print("Can't get token for", username)
-
-def getTopTracks():
-    currentTracks = sp.current_user_top_tracks(limit=20, offset=0, time_range='short_term')
-
-    listOfTracks=[]
-    for song in currentTracks["items"]:
-        artistName = song["artists"][0]["name"]
-        songName = song["name"]
-        listOfTracks.append( Track(artistName,songName) )
-    return listOfTracks
-
 if __name__ == '__main__':
-  '''
-      : param spotifyUserName : Your Spotify name
-  '''
   try:
-      spotifyUserName = "r0ym0nd"
-      playlistName = "Hot Songs"
-      topTracks = getTopTracks()
-      generate_spotify_playlist(topTracks,playlistName, spotifyUserName)
+      spotifyObsession = SpotifyObsession(SPOTIFY_API_KEY, SPOTIFY_API_SECRET, SPOTIFY_URI)
+      spotifyObsession.setUser("r0ym0nd")
+      spotifyObsession.setTargetLimit(30)
+      spotifyObsession.generateSpotifyPlaylist()
   except Exception as e:
       print(e)
